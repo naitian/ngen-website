b59d7a79c0476250ef296c66be680a8aa783ea0b
Naitian Zhou <naitian@naitian.org>
2023-05-05 16:57:47 -0700

    Add line labels

--- src/js/education.js
+++ src/js/education.js
@@ -1,5 +1,64 @@
 import * as d3 from "d3";
 
+const forceRelax = function () {
+  // only works in one dimension
+  let valueFn = (d) => d;
+  let radius = 16;
+  let iterations = 100;
+
+  function argsort(arr, fn) {
+    return arr
+      .map((item, index) => [item, index])
+      .sort((a, b) => fn(a[0], b[0]))
+      .map((d) => d[1]);
+  }
+
+  function sim(data) {
+    const nodes = data.map((d) => ({ fx: 0, y: sim.value()(d) }));
+    const force = d3
+      .forceSimulation(nodes)
+      .force("collide", d3.forceCollide(sim.radius() / 2).strength(2))
+      .force("freezedim", d3.forceX(0))
+      .force("stayclose", d3.forceY((d) => d.y).strength(1))
+      .stop();
+
+    for (let i = 0; i < sim.iterations(); ++i) force.tick();
+
+    // fix order if we need to
+    const orderedValues = nodes.sort((a, b) => d3.ascending(a.y, b.y));
+    const sortedDataIndices = argsort(data, (a, b) =>
+      d3.ascending(sim.value()(a), sim.value()(b))
+    );
+    console.log(data.map(sim.value()));
+    console.log(sortedDataIndices);
+    console.log(orderedValues.map((d) => d.y));
+
+    return sortedDataIndices.map((idx, sortedIdx) => ({
+      ...data[idx],
+      relaxed: orderedValues[sortedIdx].y
+    }));
+  }
+
+  sim.value = function (value) {
+    if (!arguments.length) return valueFn;
+    valueFn = value;
+    return sim;
+  };
+
+  sim.radius = function (value) {
+    if (!arguments.length) return radius;
+    radius = value;
+    return sim;
+  };
+
+  sim.iterations = function (value) {
+    if (!arguments.length) return iterations;
+    iterations = value;
+    return sim;
+  };
+
+  return sim;
+}
 
 const prepareData = async () => {
   const data = await d3.csv(
@@ -53,7 +112,7 @@ export class EducationChart {
       top: 15,
       right: 35,
       bottom: 20,
-      left: 40,
+      left: 60,
     }
     this.width = width;
     this.height = height;
@@ -61,6 +120,7 @@ export class EducationChart {
     this.innerHeight = this.height - this.margin.top - this.margin.bottom;
 
     this.xScale = d3.scalePoint()
+      .padding(0.2)
       .domain(this.educLevels)
       .range([0, this.innerWidth])
 
@@ -100,7 +160,6 @@ export class EducationChart {
       .x(d => this.xScale(d.EDUC_GRP))
       .y(d => this.yScale(+d.ASECWT))
 
-    console.log(slice)
     const lines = g.selectAll("path")
       .data(genGroups.map(d => [d[0], d[1].sort((a, b) => d3.ascending(
         this.educLevels.indexOf(a.EDUC_GRP),
@@ -112,6 +171,25 @@ export class EducationChart {
       .attr("fill", "none")
       .attr("opacity", (d) => d[0] === genFocus ? 1 : 0.2)
 
+    const relax = forceRelax().value(d => this.yScale(d[1][d[1].length - 1].ASECWT))
+
+    const labels = g.selectAll("text.label")
+    .data(relax(genGroups.map(d => [d[0], d[1].sort((a, b) => d3.ascending(
+      this.educLevels.indexOf(a.EDUC_GRP),
+      this.educLevels.indexOf(b.EDUC_GRP),
+    ))])))
+      .join("text")
+      .attr("class", "label")
+      .attr("x", d => this.xScale(d[1][d[1].length - 1].EDUC_GRP) + 10)
+      .attr("y", d => d.relaxed)
+      .attr("dominant-baseline", "middle")
+      .attr("fill", d => this.colorScale(d[0]))
+      .text(d => d[0])
+      // .attr("stroke", d => this.colorScale(d[0]))
+      // .attr("fill", "none")
+      // .attr("opacity", (d) => d[0] === genFocus ? 1 : 0.2)
+
+
     g.selectAll("circle")
       .data(slice)
       .join("circle")
@@ -122,7 +200,7 @@ export class EducationChart {
       .attr("opacity", (d) => d.GEN === genFocus ? 1 : 0.4)
 
     const xAxis = d3.axisBottom(this.xScale)
-    const yAxis = d3.axisLeft(this.yScale)
+    const yAxis = d3.axisLeft(this.yScale).tickFormat(d3.format(".0%"))
 
     g.selectAll("g.xaxis").data([0]).join("g")
       .attr("class", "xaxis")
